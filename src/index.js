4247f880a73e529f7834a2cd47fe5521d011d3f3
alejandro <alex4d0507@gmail.com>
2023-09-28 01:07:37 -0600

    be corregido

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import { Server, Socket } from "socket.io";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
+import SocketServer from "./SocketServer.js";
 
 //env variables
 const { DATABASE_URL } = process.env;
@@ -31,8 +33,21 @@ server = app.listen(PORT, () => {
     logger.info(`Server is listening at ${PORT}.`);
 });
 
-//handle server errors
+//socket io
+const io = new Server (server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: process.env.CLIENT_ENDPOINT,
+    },
+});
+io.on("connection", (socket) => {
+    logger.info("socket io connected successfully.");
+    SocketServer(socket, io);
+});
 
+
+
+//handle server errors
 const exitHandler = () => {
     if(server) {
     logger.info("Server closed.");
