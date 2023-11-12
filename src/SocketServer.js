8123708a9861f0cef8eae355eb035ab849b7f469
alejandro <alex4d0507@gmail.com>
2023-11-12 00:42:10 -0600

    Corregido pro

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,47 +1,66 @@
 let onlineUsers = [];
 export default function (socket, io) {
-    //user joins or opens the application
-    socket.on("join", (user) => {
-        console.log("user has joined : ", user);
-        socket.join(user);
-        //add joined user to online users
-        if(!onlineUsers.some((u) =>u.userId===user)){
-            console.log(`user ${user} is now online.`);
-            onlineUsers.push({userId: user, socketId: socket.id});
-        }
-        //send online users to frontend
-        io.emit("get-online-users", onlineUsers);
-    });
+  //user joins or opens the application
+  socket.on("join", (user) => {
+    socket.join(user);
+    //add joined user to online users
+    if (!onlineUsers.some((u) => u.userId === user)) {
+      onlineUsers.push({ userId: user, socketId: socket.id });
+    }
+    //send online users to frontend
+    io.emit("get-online-users", onlineUsers);
+    //send socket id
+    io.emit("setup socket", socket.id);
+  });
 
-    //socket disconnect
-    socket.on("disconnect", () => {
-        onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
-        io.emit("get-online-users", onlineUsers);
-    });
+  //socket disconnect
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("get-online-users", onlineUsers);
+  });
 
-    
+  //join a conversation room
+  socket.on("join conversation", (conversation) => {
+    socket.join(conversation);
+  });
 
+  //send and receive message
+  socket.on("send message", (message) => {
+    let conversation = message.conversation;
+    if (!conversation.users) return;
+    conversation.users.forEach((user) => {
+      if (user._id === message.sender._id) return;
+      socket.in(user._id).emit("receive message", message);
+    });
+  });
 
-    //join a conversation room
-    socket.on("join conversation", (conversation) => {
-        socket.join(conversation);
-    });
+  //typing
+  socket.on("typing", (conversation) => {
+    socket.in(conversation).emit("typing", conversation);
+  });
+  socket.on("stop typing", (conversation) => {
+    socket.in(conversation).emit("stop typing");
+  });
 
-    //send and receive message
-    socket.on("send message", (message) => {
-        let conversation = message.conversation;
-        if (!conversation.users) return;
-        conversation.users.forEach((user) => {
-            if (user._id === message.sender._id) return;
-            socket.in(user._id).emit("receive message", message);
-        });
+  //call
+  //---call user
+  socket.on("call user", (data) => {
+    let userId = data.userToCall;
+    let userSocketId = onlineUsers.find((user) => user.userId == userId);
+    io.to(userSocketId.socketId).emit("call user", {
+      signal: data.signal,
+      from: data.from,
+      name: data.name,
+      picture: data.picture,
     });
+  });
+  //---answer call
+  socket.on("answer call", (data) => {
+    io.to(data.to).emit("call accepted", data.signal);
+  });
 
-    //typing
-    socket.on("typing", (conversation)=> {
-        socket.in(conversation).emit("typing", conversation);
-    });
-    socket.on("stop typing", (conversation)=> {
-        socket.in(conversation).emit("stop typing");
-    });
+  //---end call
+  socket.on("end call", (id) => {
+    io.to(id).emit("end call");
+  });
 }
--- src/controllers/conversation.controller.js
+++ src/controllers/conversation.controller.js
@@ -1,7 +1,6 @@
 import createHttpError from "http-errors";
 import logger from "../configs/logger.config.js";
 import { createConversation, doesConversationExist, populatedConversation, getUserConversations, } from "../services/conversation.service.js";
-import { findUser } from "../services/user.service.js";
 
 export const create_open_conversation = async (req, res, next) => {
     try {
@@ -47,6 +46,40 @@ export const getConversations = async (req, res, next) => {
         const user_id = req.user.userId;
         const conversations = await getUserConversations(user_id);
         res.status(200).json(conversations);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const createGroup = async (req, res, next) => {
+    const {name, users} = req.body;
+    //add current user to users
+    users.push(req.user.userId);
+    if(!name || !users){
+     throw createHttpError.BadRequest("Porfavor rellena todos los campo.");
+    }
+    if(users.length<2){
+        throw createHttpError.BadRequest(
+            "Se requieren al menos 2 usuarios para iniciar un chat grupal."
+            );
+    }
+    let convoData = {
+        name,
+        users,
+        isGroup: true,
+        admin: req.user.userId,
+        picture: process.env.DEFAULT_GROUP_PICTURE,
+    };
+    try {
+        const newConvo=await createConversation(convoData);
+        const populatedConvo = await populatedConversation(
+            newConvo._id,
+            "users admin",
+            "-password"
+        );
+        console.log(populatedConvo);
+        res.status(200).json(populatedConvo);
     } catch (error) {
         next(error);
     }
